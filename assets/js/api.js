eca9b2c7790c43025505206ff441340d2b95c0e7
Thiago Barros <thiago.lusa@yahoo.com.br>
2026-03-14 00:48:40 -0300

    Correção na chamada da url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/barros-thiago/portfolio/refs/heads/mainassets/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/barros-thiago/portfolio/refs/heads/main/assets/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
